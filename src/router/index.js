4eae61632092df13fd5ce2a1994a91f47b68c3a1
wouerner <wouerner@protonmail.com>
2020-10-30 11:59:41 -0300

    init

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "../views/Home";
+import About from "../views/About";
+
+const routes = [
+  {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+  {
+      path: "/about",
+      name: "About",
+      component: About,
+    },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
